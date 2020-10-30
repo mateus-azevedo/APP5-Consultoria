ee3770af5df0c77c7913bfb579918cd3751de848
Mateus Santos <m.santos.azeve@gmail.com>
2020-10-29 22:49:34 -0300

    entendendo o navigator

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-import React, { Component } from 'react';
-import {
-  AppRegistry
-} from 'react-native';
-import { Navigator } from 'react-native-deprecated-custom-components'
+import React, {Component} from 'react';
+import {AppRegistry} from 'react-native';
+import {Navigator} from 'react-native-deprecated-custom-components';
 import CenaPrincipal from './src/components/CenaPrincipal';
 import CenaClientes from './src/components/CenaClientes';
 
 export default class app5 extends Component {
   render() {
-    return(
-      <Navigator 
-        initialRoute={/* objeto routa */}
+    return (
+      <Navigator
+        initialRoute={{id: 'a'}}
         renderScene={(route, navigator) => {
           /* definir a Cena com base na rota */
+          if (route.id === 'a') {
+            // exibir a CenaPrincipal
+            return <CenaPrincipal />;
+          }
+
+          if (route.id === 'b') {
+            // exibir a CenaClientes
+            return <CenaClientes />;
+          }
         }}
       />
     );
