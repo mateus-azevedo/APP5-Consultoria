1c7a71f1fad82c64892334aa35bcb611292bc918
Mateus Santos <m.santos.azeve@gmail.com>
2020-10-29 22:39:46 -0300

    codificando o navigator

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import {
   AppRegistry
 } from 'react-native';
-
+import { Navigator } from 'react-native-deprecated-custom-components'
+import CenaPrincipal from './src/components/CenaPrincipal';
 import CenaClientes from './src/components/CenaClientes';
 
 export default class app5 extends Component {
   render() {
     return(
-      <CenaClientes />
+      <Navigator 
+        initialRoute={/* objeto routa */}
+        renderScene={(route, navigator) => {
+          /* definir a Cena com base na rota */
+        }}
+      />
     );
   }
 }
