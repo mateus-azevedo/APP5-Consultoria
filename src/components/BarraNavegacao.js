d2deb318c08b026e1495fcba068d5f8d2528f183
Mateus Santos <m.santos.azeve@gmail.com>
2020-11-07 10:42:30 -0300

    Implementado o botão de retorno da navegação

--- src/components/BarraNavegacao.js
+++ src/components/BarraNavegacao.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import {
   Text,
   View,
-  StyleSheet
+  StyleSheet,
+  Image
 } from 'react-native';
+
+const btnVoltar = require('../imgs/btn_voltar.png');
 
 export default class BarraNavegacao extends Component {
   render() {
+    if(this.props.voltar) {
+      return(
+        <View style={styles.barraTitulo}>
+          <Image source={btnVoltar} />
+          <Text style={styles.titulo}>ATM Consultoria</Text>
+        </View>
+      );
+    }
+
     return(
       <View style={styles.barraTitulo}>
         <Text style={styles.titulo}>ATM Consultoria</Text>
@@ -19,7 +31,8 @@ const styles = StyleSheet.create({
   barraTitulo: {
     backgroundColor: '#CCC',
     padding: 10,
-    height: 60
+    height: 60,
+    flexDirection: 'row'
   },
   titulo: {
     flex: 1, // faz o component titulo ocupar o espaço completo da tela
